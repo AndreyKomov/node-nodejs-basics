aeefdcdc0dbb8939ceeeedd0d0fedfa9de1ebc53
andreykomov <komov.te@gmail.com>
2024-10-06 00:06:23 +0200

    [task-0001] implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+// var fs = require('fs');
+import * as fs from 'fs';
+
+const freshData = 'I am fresh and young';
+const filePath = 'src/fs/files/fresh.txt';
 const create = async () => {
-    // Write your code here 
+    fs.access(filePath, fs.constants.F_OK, (error) => {
+        if (!error) {
+            throw new Error('FS operation failed');
+        }
+    });
+
+    fs.writeFile(filePath, freshData, (error) => {
+     if (error) {
+        throw new Error(`Something went wrong: ${error}`);
+    }
+    });
 };
 
 await create();
