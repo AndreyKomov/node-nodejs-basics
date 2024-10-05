3e6fab00ffdafd2a48e1d376d171e757a35ffd34
andreykomov <komov.te@gmail.com>
2024-10-06 00:19:53 +0200

    [task-0001] implement copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import * as fs from 'fs';
+
+const sourceFolderPath = 'src/fs/files';
+const destinationFolderPath = 'src/fs/files_copy';
+
 const copy = async () => {
-    // Write your code here 
+    fs.cp(
+        sourceFolderPath,
+        destinationFolderPath,
+        {errorOnExist: true, force: false, recursive: true},
+        (error) => {
+        if (error) {
+            if (error.code === 'ERR_FS_CP_EEXIST') {
+                throw new Error('FS operation failed');
+            } else {
+                throw error;
+            }
+        }
+    })
 };
 
 await copy();
