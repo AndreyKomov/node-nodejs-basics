78263596b3d7186225b3bda2a53f69bce9f2bb1b
andreykomov <komov.te@gmail.com>
2024-10-06 19:57:58 +0200

    [task-0001] fixed args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,8 +1,8 @@
 import { argv } from 'node:process';
 
 const parseArgs = () => {
-    const args = process.argv;
-
+    const args = process.argv.slice(2);
+    
     console.log(args.map((item, index) => index%2 ? `is ${item},` : item.slice(2)).join(' '));
     
 };
