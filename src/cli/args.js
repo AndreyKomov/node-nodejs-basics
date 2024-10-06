16d6f956616eff6325fd845f464b7bb2dbdb529c
andreykomov <komov.te@gmail.com>
2024-10-06 20:11:50 +0200

    [task-0001] implemented transformStream

--- src/cli/args.js
+++ src/cli/args.js
@@ -4,7 +4,6 @@ const parseArgs = () => {
     const args = process.argv.slice(2);
     
     console.log(args.map((item, index) => index%2 ? `is ${item},` : item.slice(2)).join(' '));
-    
 };
 
 parseArgs();
