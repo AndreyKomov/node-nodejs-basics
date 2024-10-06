3f1b576b19e8334338c86a13194845c74e1062e8
andreykomov <komov.te@gmail.com>
2024-10-06 18:01:24 +0200

    [task-0001] implemented read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import * as fs from 'fs';
+
+const readedFilePath = 'src/fs/files/fileToRead.txt';
 const read = async () => {
-    // Write your code here 
+    fs.readFile(readedFilePath, 'utf8', (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                throw new Error('FS operation failed');
+            } else {
+                throw new Error(`Something went wrong: ${err}`);
+            }
+        }
+
+        console.log(`The file content is: ${content}`);
+    })
 };
 
 await read();
