f20978fec2639947ca77413348cecd3451308f96
andreykomov <komov.te@gmail.com>
2024-10-06 19:51:12 +0200

    [task-0001] implemented writeStream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,10 @@
+import * as fs from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const filePath = 'src/streams/files/fileToWrite.txt';
+    const stdin = process.stdin;
+
+    stdin.pipe(fs.createWriteStream(filePath, 'utf-8'));
 };
 
 await write();
