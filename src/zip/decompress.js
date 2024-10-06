1dad2ee4cb0722350f2e8fea53ee39a93c97818e
andreykomov <komov.te@gmail.com>
2024-10-06 23:46:30 +0200

    [task-0001] implemented decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { pipeline } from 'node:stream';
+import { createGunzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from'node:fs';
+
 const decompress = async () => {
-    // Write your code here 
+    const gunzip = createGunzip();
+    const decompressedFilePath = createReadStream('src/zip/files/archive.gz');
+    const arhiveFilePath = createWriteStream('src/zip/files/fileToCompress.txt');
+
+    pipeline(decompressedFilePath, gunzip, arhiveFilePath, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = -1;
+        }
+    });
 };
 
 await decompress();
