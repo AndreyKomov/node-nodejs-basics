d6cb8c8a938bb6fdcdbac4809b38aa205f479603
andreykomov <komov.te@gmail.com>
2024-10-06 18:32:11 +0200

    [task-0001] implemented env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    const currentEnv = process.env;
+    const filteredKeysVariables = Object.keys(currentEnv).filter(item => item.startsWith('RSS_'));
+    const result = filteredKeysVariables.map((item) => `${item}=${currentEnv[item]};`);
+
+    console.log(`RSS env variables: ${result.join(' ')}`);
 };
 
 parseEnv();
