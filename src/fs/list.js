67e55026630b1f46ce2826fdc70d45c207e9a441
andreykomov <komov.te@gmail.com>
2024-10-06 16:58:23 +0200

    [task-0001] implemented list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import * as fs from 'fs';
+
+const filesFolderPath = 'src/fs/files';
 const list = async () => {
-    // Write your code here 
+    fs.readdir(filesFolderPath, (err, fileList) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                throw new Error('FS operation failed');
+            } else {
+                throw new Error(`Something went wrong: ${err}`)
+            }
+        }
+
+        console.log(`Files list: ${fileList}`);
+    });
 };
 
 await list();
