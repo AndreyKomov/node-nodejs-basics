4fd13c2979c9578e7b306a6b7abddaa744a9976e
andreykomov <komov.te@gmail.com>
2024-10-06 15:53:45 +0200

    [task-0001] implement rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,13 @@
+import * as fs from 'fs';
+
+const renamedFilePath = 'src/fs/files/wrongFilename.txt';
+const properFilePath = 'src/fs/files/properFilename.md';
+
 const rename = async () => {
-    // Write your code here 
+    fs.rename(renamedFilePath, properFilePath, (err) => {
+        if (err) throw new Error('FS operation failed');
+        console.log('Rename complete!');
+    });
 };
 
 await rename();
