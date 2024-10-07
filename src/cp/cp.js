0f93ca91c7c4b6a7030623c9b71c46c7532f1017
Andrei Komau <andrei_komau@epam.com>
2024-10-07 15:18:23 +0200

    [task-0001] implemented child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import {fork} from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptPath = 'src/cp/files/script.js';
+    const childProcess = fork(
+        scriptPath,
+        args,
+        {
+            stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+        }
+    );
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
+    childProcess.stderr.pipe(process.stderr);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( /* [someArgument1, someArgument2, ...] */['qq', 2]);
